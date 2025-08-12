775ef0f62298d4579e79c3f4327aac3d8b149b7f
amilton0656 <amilton0656@gmail.com>
2025-08-12 17:30:10 -0300

    xx

---
+++ intranet/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from portal.views import home
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="registration/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(next_page='login'),  # garante o redirect
+        name="logout",
+    ),
+    path("", home, name="home"),
+]
