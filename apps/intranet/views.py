783e849d16bff7d83b1425fbb17bc294a515c929
amilton0656 <amilton0656@gmail.com>
2025-09-24 14:14:01 -0300

    xxx

--- apps/intranet/views.py
+++ apps/intranet/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from uteis import Uteis
 
 
+@login_required
 def intranet_home(request):
     uteis = Uteis()
     cubs = uteis.cubs_hoje()
